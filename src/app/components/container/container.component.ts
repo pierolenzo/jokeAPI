a8f08f2d433b33ee7fb73494311f9b5820219574
pierolenzo <pierolenzo@gmail.com>
2021-10-25 15:11:22 +0200

    refactor: refactor service

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,7 +1,6 @@
 import { GetJokeService } from './../../core/services/get-joke.service';
 import { Component, OnInit } from '@angular/core';
-import { Subscription, Observable, of } from 'rxjs';
-import { mergeMap, delay, repeat } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-container',
@@ -10,20 +9,15 @@ import { mergeMap, delay, repeat } from 'rxjs/operators';
 })
 export class ContainerComponent implements OnInit {
   private subscription!: Subscription;
-  private DELAY: number;
   public joke: any;
 
   constructor(private getJokeService: GetJokeService) {
-    this.DELAY = 5000;
     this.joke = {};
   }
 
   ngOnInit(): void {
-    this.subscription = of({}).pipe(
-      mergeMap((_) => this.getJokeService.get()),
-      delay(this.DELAY),
-      repeat()
-    ).subscribe(data => this.joke = data)
+    this.subscription = this.getJokeService.poll()
+      .subscribe(data => this.joke = data)
   }
 
   /**
@@ -31,7 +25,7 @@ export class ContainerComponent implements OnInit {
    */
   public stop() {
     this.subscription.unsubscribe();
-    console.log('Stop eseguito!');
+    console.log('(unsubscribe) - Stop eseguito!');
   }
 
   ngOnDestroy(): void {
--- src/app/core/services/get-joke.service.ts
+++ src/app/core/services/get-joke.service.ts
@@ -1,27 +1,44 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
-import { catchError, delay, mergeMap, repeat, retry, tap } from 'rxjs/operators';
+import {
+  catchError,
+  delay,
+  mergeMap,
+  repeat,
+  retry,
+} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GetJokeService {
   private URL: string;
+  private DELAY: number;
 
   constructor(private http: HttpClient) {
     this.URL = 'https://v2.jokeapi.dev/joke/any';
+    this.DELAY = 5000;
   }
 
   /**
-   * get() Get jokes from API
+   * get() - Get jokes from API
    */
   public get(): Observable<any> {
-
     return this.http.get<any>(this.URL).pipe(
       retry(3),
-      tap(() => console.log('get eseguito')),
       catchError(this.handleError)
+    );
+  }
+
+  /**
+   * poll() - Executes get()
+   */
+  public poll(): Observable<any> {
+    return of({}).pipe(
+      mergeMap((_) => this.get()),
+      delay(this.DELAY),
+      repeat()
     );
   }
 
